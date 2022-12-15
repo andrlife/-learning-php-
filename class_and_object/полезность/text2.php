bd5178a335536beeb1696b98aacd802294e648d0
Andrii <105038628+andrlife@users.noreply.github.com>
2022-12-15 23:32:29 +0200

    Create text2.php

---
+++ class_and_object/полезность/text2.php
@@ -0,0 +1,7 @@
+<?php
+
+echo 'Это текст находящийся в папке utilite file text2.php';
+    echo '<br>';
+    echo 'require_once и прописан абсолютный путь путь C:/OSPanel/domains/local.php/Классы и обьекты/utilite/text2.php';
+
+?>
